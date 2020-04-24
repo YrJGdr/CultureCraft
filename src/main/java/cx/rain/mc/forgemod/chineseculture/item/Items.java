31cfdde476c22ddf8afaa4e6c67ff142a9b6e30c
秋雨落 <i@rain.cx>
2020-04-24 08:34:26 +0800

    修了一个bug发现一个新bug
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/forgemod/chineseculture/item/Items.java
@@ -0,0 +1,17 @@
+package cx.rain.mc.forgemod.chineseculture.item;
+
+import cx.rain.mc.forgemod.chineseculture.ChineseCulture;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+
+@EventBusSubscriber(modid = ChineseCulture.MODID)
+public class Items {
+    @SubscribeEvent
+    public static void onRegisterItem(RegistryEvent.Register<Item> event) {
+        // Register items manually.
+
+    }
+}
--- src/main/java/cx/rain/mc/forgemod/chineseculture/item/automatic/ItemDumpling.java
+++ src/main/java/cx/rain/mc/forgemod/chineseculture/item/automatic/ItemDumpling.java
@@ -1,13 +1,12 @@
 package cx.rain.mc.forgemod.chineseculture.item.automatic;
 
+import cx.rain.mc.forgemod.chineseculture.ChineseCulture;
 import net.minecraft.item.ItemFood;
 
-
 public class ItemDumpling extends ItemFood {
-    public ItemDumpling(int hungerHeal, float saturation, boolean isWolfFood) {
+    public ItemDumpling() {
         super(8, 0.8F, false);
-        this.setUnlocalizedName("dumpling");
+        setUnlocalizedName("dumpling");
+        setRegistryName(ChineseCulture.MODID, "dumpling");
     }
-
-
 }
