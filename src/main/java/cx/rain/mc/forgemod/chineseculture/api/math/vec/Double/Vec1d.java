49f993c22cb3d7b456bb56df6dc836f314f17933
theFlysong <song_of_the_fly@163.com>
2020-06-20 22:32:35 +0800

    Add green_radish.png,radish.png,ternip.png Refact charred_food.png

---
+++ src/main/java/cx/rain/mc/forgemod/chineseculture/api/math/vec/Double/Vec1d.java
@@ -0,0 +1,12 @@
+package cx.rain.mc.forgemod.chineseculture.api.math.vec.Double;
+
+/**
+ * @author Infinity_rain
+ */
+public class Vec1d {
+    public double x;
+
+    public Vec1d(double x){
+        this.x=x;
+    }
+}
---
+++ src/main/java/cx/rain/mc/forgemod/chineseculture/api/math/vec/Float/Vec1f.java
@@ -0,0 +1,12 @@
+package cx.rain.mc.forgemod.chineseculture.api.math.vec.Float;
+
+/**
+ * @author Infinity_rain
+ */
+public class Vec1f {
+    public float x;
+
+    public Vec1f(float x){
+        this.x=x;
+    }
+}
